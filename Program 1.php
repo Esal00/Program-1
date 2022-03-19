8821e4e16942e28bd34a95ef545b215c935fdcf5
Esal00 <101726530+Esal00@users.noreply.github.com>
2022-03-19 22:08:56 +0700

    Add files via upload

--- Program 1.php
+++ Program 1.php
@@ -1,1 +1,33 @@
-jmkmjs,akdmkams
+<?php
+class buah {
+  // Properties
+  public $nama;
+  public $warna;
+
+  // Methods
+  function set_nama($nama) {
+    $this->nama = $nama;
+  }
+  function get_nama() {
+    return $this->nama;
+  }
+  function set_warna($warna) {
+    $this->warna = $warna;
+  }
+  function get_warna() {
+    return $this->warna;
+  }
+}
+
+$apel = new buah();
+$apel->set_nama('apel');
+$apple->set_warna('merah');
+echo "Nama: " . $apel->get_nama();
+echo "<br>";
+echo "Warna: " . $apel->get_warna();
+?>
+  
+  /*Output
+  Nama: apel
+  Warna: merah
+  */
